9003e36271ab328197fb5dfc0574c3b475ef4446
danny <danny@sport195.com>
2014-02-10 10:55:06 -0600

    added max-width

--- pitch.js
+++ pitch.js
@@ -1,11 +1,11 @@
 db = {};
 db.session = new Meteor.Collection( 'session' );
 db.gigs = new Meteor.Collection( 'gigs' );
-db.gigState = new Meteor.Collection( 'gigstate' );
+db.gigstate = new Meteor.Collection( 'gigstate' );
 
 pitch = {
-	setContentHeight: function ( ) {
-		var height = $( window ).height( );
+	setContentHeight: function () {
+		var height = $( window ).height();
 		$( ".content" ).height( height - ( contentMargin * 3 ) );
 	}
 }
@@ -24,9 +24,9 @@ if ( Meteor.isClient ) {
 		setHeights = true;
 
 	var pitch = {
-		setContentHeight: function ( ) {
+		setContentHeight: function () {
 			if ( setHeights ) {
-				var height = verge.viewportH( );
+				var height = verge.viewportH();
 				$( ".content" ).height( height - ( contentMargin * 2 ) );
 			}
 		}
@@ -34,21 +34,21 @@ if ( Meteor.isClient ) {
 
 	// Adjust columns according to breakpoints
 	enquire.register( "screen and (max-width: 767px)", {
-		match: function ( ) {
+		match: function () {
 			Session.set( 'numColumns', 1 );
 			setHeights = false;
 		}
 	} )
 
 	.register( "screen and (min-width: 768px) and (max-width: 1024px)", {
-		match: function ( ) {
+		match: function () {
 			Session.set( 'numColumns', 2 );
 			setHeights = true;
 		}
 	} )
 
 	.register( "screen and (min-width: 1024px)", {
-		match: function ( ) {
+		match: function () {
 			Session.set( 'numColumns', 3 );
 			setHeights = true;
 		}
@@ -58,36 +58,36 @@ if ( Meteor.isClient ) {
 	$( window ).resize( pitch.setContentHeight );
 
 	// feed the gigs
-	Template.gigs.rows = function ( ) {
+	Template.gigs.rows = function () {
 
 		// fetch all the gigs, making sure an active state is set
-		var gigs = _( db.gigs.find( {} ).fetch( ) ).map( function ( gig ) {
+		var gigs = _( db.gigs.find( {} ).fetch() ).map( function ( gig ) {
 			return _( gig ).defaults( {
 				active: 'description'
 			} )
 		} );
 
 		// group gigs into rows
-		return _( gigs ).chain( ).groupBy( function ( gig, i ) {
+		return _( gigs ).chain().groupBy( function ( gig, i ) {
 			return Math.floor( i / Session.get( 'numColumns' ) );
-		} ).values( ).value( );
+		} ).values().value();
 
 	};
 
-	Template.gig.active = function ( ) {
+	Template.gig.active = function () {
 		// TODO set active state from gigstate record
 	};
 
-	Template.gigRow.rendered = function ( ) {
+	Template.gigRow.rendered = function () {
 		// set content heights
-		pitch.setContentHeight( );
+		pitch.setContentHeight();
 
 		// match height of headers across row
 		var headers = this.findAll( 'h3' ),
 			max = _( headers ).max( function ( el ) {
-				return $( el ).height( );
+				return $( el ).height();
 			} ),
-			height = $( max ).height( );
+			height = $( max ).height();
 
 		_( headers ).each( function ( el ) {
 			$( el ).height( height );
@@ -125,7 +125,7 @@ if ( Meteor.isClient ) {
 
 if ( Meteor.isServer ) {
 
-	Meteor.startup( function ( ) {
+	Meteor.startup( function () {
 
 		// reset gigs
 		db.gigs.remove( {} );
